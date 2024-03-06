b6fe05cd31726186c4a516c1c70b410ac14ab744
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-06 22:59:06 +0200

    log api requests

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
