e40d07dce00ae629d9711c18efbd405b97d1bf9c
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-07 22:08:42 +0200

    add register functonality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,23 @@
 const User = require('../models/User');
 
 const registerUser = async (req, res) => {
-  const user = req.body;
-  const newUser = await User.create(user);
-  res.status(201).json(newUser);
+  const { username, email, password } = req.body;
+  const dupUser = await User.findOne({ email: email });
+  if (dupUser) {
+    return next(new Error('user already exist'));
+  }
+  const newUser = await User.create({
+    username: username,
+    email: email,
+    password: password,
+  });
+  res.status(201).json({
+    status: 'success',
+    newUser,
+  });
 };
 
 
-
-
 module.exports = {
-  registerUser
-}
+  registerUser,
+};
